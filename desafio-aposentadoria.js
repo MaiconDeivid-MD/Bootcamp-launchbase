05fe6eb3382f92c62176e2bf951031bc2817e10b
Maicon <maicondeivid_@hotmail.com>
2020-08-30 16:28:36 -0300

    commit

---
+++ desafio-aposentadoria.js
@@ -0,0 +1,20 @@
+// DESAFIO LAUNCHBASE CALCULO DE APOSENTADORIA
+
+
+const nome = 'Willian'
+const sexo = 'M'
+const idade = 51
+const contribuicao = 23
+
+const calculoContribuicao = idade + contribuicao
+
+// essas variáveis ​​irão retornar verdadeiro ou falso
+
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+if (homemPodeAposentar || mulherPodeAposentar) {
+    console.log(`${ nome } , você pode se aposentar!`)
+} else {
+    console.log(`${ nome } , você não pode se aposentar!`)
+}
---
+++ desafio-imc.js
@@ -0,0 +1,17 @@
+
+
+const nome = 'maicon'
+const peso = 110
+const altura  = 1.88
+
+const imc = (peso / ( altura * altura ));
+
+mensagem = ``
+
+if  ( imc >= 30 ) {
+   mensagem  = (`${ nome } você está acima do peso`)
+}  else {
+   mensagem  = (`${ nome } você não está acima do peso`)
+}
+
+console.log ( mensagem )
